e156844743e0010bd7fd8fb9e2349c8c6e735e96
Reysmer Valle <reysmerwvr@gmail.com>
2020-07-09 22:24:31 -0500

    Update Dockerfile | Fix Docker Spin Up

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
-pipenv install 
+if [ "$DB_CONNECTION" = "mysql" ]
+then
+    echo "Waiting for mysql..."
+
+    while ! nc -z $DB_HOST $DB_PORT; do
+        sleep 0.1
+    done
+
+    echo "MySQL started"
+fi
+
+pipenv run python manage.py flush --no-input
 pipenv run python manage.py migrate
-pipenv run python manage.py runserver 8000
+
+exec "$@"
